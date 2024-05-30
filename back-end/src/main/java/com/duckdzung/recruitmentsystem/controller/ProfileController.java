241269837928b55cfdad18b17fb099481f6118f0
duckdzung <laiducdung1911@gmail.com>
2024-05-30 13:14:13 +0700

    config cors

--- back-end/src/main/java/com/duckdzung/recruitmentsystem/controller/ProfileController.java
+++ back-end/src/main/java/com/duckdzung/recruitmentsystem/controller/ProfileController.java
@@ -13,6 +13,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/api/profiles")
 public class ProfileController {
