ae00682044d15bdeaddece9ea70c6d4c5b8db280
duckdzung <laiducdung1911@gmail.com>
2024-06-02 14:22:36 +0700

    update routing

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,21 +1,24 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import PrivateRoute from './routes/PrivateRoute';
-import PublicRoute from './routes/PublicRoute';
-import Home from './pages/Home/Home';
-import Authentication from './pages/Authentication/Authentication';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import UpdateMember from './components/UpdateMember';
+
+import PublicRoute from './routes/PublicRoute';
+import ProtectedRoute from './routes/ProtectedRoute';
+
+import Home from './pages/Home/Home';
+import Authentication from './pages/Authentication/Authentication';
 import Recruitment from './pages/Recruitment/Recruitment';
-import AdminRoute from './routes/AdminRoute';
-import CandidateListing from './components/CandidateListing';
-import PaymentDetails from './components/PaymentDetails';
+import UpdateMember from './components/UpdateMember';
+
+import { Role } from './types';
+import AdminPage from './components/admin/AdminPage';
 
 const App: React.FC = () => {
     return (
         <Router>
             <Routes>
+                {/* Public routes */}
                 <Route element={<PublicRoute restricted={true} />}>
                     <Route path="/login" element={<Authentication />} />
                     <Route path="/register" element={<Authentication />} />
@@ -23,18 +26,19 @@ const App: React.FC = () => {
 
                 <Route element={<PublicRoute restricted={false} />}>
                     <Route path="/" element={<Home />} />
-                    <Route path="/recruitment" element={<Recruitment />} />
                 </Route>
 
-                <Route element={<PrivateRoute />}>
+                {/* Protected routes */}
+                <Route element={<ProtectedRoute allowRoutes={[Role.MEMBER]} />}>
                     <Route path="/update-member" element={<UpdateMember />} />
                 </Route>
 
-                <Route element={<AdminRoute />}>
-                    <Route path="/admin/candidate-listing" element={<CandidateListing />} />
+                <Route element={<ProtectedRoute allowRoutes={[Role.ENTERPRISE]} />}>
+                    <Route path="/recruitment" element={<Recruitment />} />
                 </Route>
-                <Route element={<AdminRoute />}>
-                    <Route path="/admin/payment-details" element={<PaymentDetails />} />
+
+                <Route element={<ProtectedRoute allowRoutes={[Role.STAFF, Role.PRESIDENT]} />}>
+                    <Route path="/admin/account" element={<AdminPage />} />
                 </Route>
             </Routes>
             <ToastContainer />
